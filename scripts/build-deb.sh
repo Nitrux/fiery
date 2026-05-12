8fc3b83cbd035c4a84b35e695592e9b5b8990ba7
Uri Herrera <hello@uriherrera.com>
2026-05-12 17:47:56 -0600

    update pkg dep

--- scripts/build-deb.sh
+++ scripts/build-deb.sh
@@ -63,7 +63,7 @@ checkinstall -D -y \
 	--pakdir=. \
 	--maintainer=uri_herrera@nxos.org \
 	--provides=fiery \
-	--requires="gir1.2-secret-1,kio-extras,libqt6pdf6,libqt6pdfquick6,libqt6pdfwidgets6,libqt6webenginecore6,libqt6webenginecore6-bin,libqt6webenginequick6,libqt6webenginewidgets6,libsecret-1-0,mauikit \(\>= 4.0.2\),mauikit-filebrowsing \(\>= 4.0.2\),qml6-module-qtcore,qml6-module-qtquick-effects,qml6-module-qtquick-pdf,qml6-module-qtwebengine,qml6-module-qtwebengine-controlsdelegates" \
+	--requires="gir1.2-secret-1,kio-extras,libqt6pdf6,libqt6pdfquick6,libqt6pdfwidgets6,libqt6webenginecore6,libqt6webenginecore6-bin,libqt6webenginequick6,libqt6webenginewidgets6,libsecret-1-0,mauikit \(\>= 4.0.3\),mauikit-filebrowsing \(\>= 4.0.3\),qml6-module-qtcore,qml6-module-qtquick-effects,qml6-module-qtquick-pdf,qml6-module-qtwebengine,qml6-module-qtwebengine-controlsdelegates" \
 	--nodoc \
 	--strip=no \
 	--stripso=yes \
